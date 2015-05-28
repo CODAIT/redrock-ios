a66852eb27b5ede1eb54cc14ac73e237d39c287a
Jonathan Alter <jonalter@users.noreply.github.com>
2015-05-28 09:10:38 -0700

    Adding scroll view

--- Spark Insights/ViewController.swift
+++ Spark Insights/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dummyView: UIView!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var footerView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+        self.setupScrollView()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func setupScrollView() {
+        var colors = [UIColor.blueColor(), UIColor.darkGrayColor(), UIColor.grayColor()]
+        var numberOfViews = 3
+        
+        for i in 0..<numberOfViews {
+            var myOrigin = CGFloat(i) * self.dummyView.frame.size.width
+            
+            var myView = UIView(frame: CGRectMake(myOrigin, 0, self.dummyView.frame.size.width, self.dummyView.frame.size.height))
+            myView.backgroundColor = colors[i % numberOfViews]
+            
+            self.scrollView.addSubview(myView)
+        }
+        
+        self.scrollView.contentSize = CGSizeMake(self.dummyView.frame.size.width * CGFloat(numberOfViews), self.dummyView.frame.size.height)
+    }
 }
 
