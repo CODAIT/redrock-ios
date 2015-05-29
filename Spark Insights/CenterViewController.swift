afd1cf56a71941fef63ae17ad725a0f6ce546366
Rosstin <asterazul@gmail.com>
2015-05-29 14:58:11 -0700

    Transform data differently for different webviews

--- Spark Insights/CenterViewController.swift
+++ Spark Insights/CenterViewController.swift
@@ -18,6 +18,10 @@ class CenterViewController: UIViewController, UIWebViewDelegate {
 
     var delegate: CenterViewControllerDelegate?
     
+    // the name of the HTML file corresponding to a visualization in /Visualizations
+    let numberOfViews = 3
+    let visualizationNames = ["treemap", "circlepacking", "worddistance"]
+    
     @IBOutlet weak var dummyView: UIView!
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var footerView: UIView!
@@ -34,32 +38,25 @@ class CenterViewController: UIViewController, UIWebViewDelegate {
         // Dispose of any resources that can be recreated.
     }
 
+    /*
+        Creates UIWebViews for the 3 views
+    */
     func setupScrollView() {
         var colors = [UIColor.blueColor(), UIColor.darkGrayColor(), UIColor.grayColor()]
         
-        
-        // REQUESTS
-        let treeFilePath = NSBundle.mainBundle().URLForResource("Visualizations/treemap", withExtension: "html")
-        let treeRequest = NSURLRequest(URL: treeFilePath!)
-        
-        let greenFilePath = NSBundle.mainBundle().URLForResource("Visualizations/circlepacking", withExtension: "html")
-        let greenRequest = NSURLRequest(URL: greenFilePath!)
-        
-        let blueFilePath = NSBundle.mainBundle().URLForResource("Visualizations/blue", withExtension: "html")
-        let blueRequest = NSURLRequest(URL: blueFilePath!)
-        
-        var requests = [treeRequest, greenRequest, blueRequest]
-        
-        var numberOfViews = 3
-        
         for i in 0..<numberOfViews {
+            
+            let filePath = NSBundle.mainBundle().URLForResource("Visualizations/"+visualizationNames[i], withExtension: "html")
+            let request = NSURLRequest(URL: filePath!)
+            
             var myOrigin = CGFloat(i) * self.dummyView.frame.size.width
             
             var myWebView = UIWebView(frame: CGRectMake(myOrigin, 0, self.dummyView.frame.size.width, self.dummyView.frame.size.height))
             myWebView.backgroundColor = colors[i % numberOfViews]
             
-            myWebView.loadRequest(requests[i])
+            myWebView.loadRequest(request)
             
+            // set the delegate so data can be loaded in
             myWebView.delegate = self
             
             self.scrollView.addSubview(myWebView)
@@ -69,22 +66,38 @@ class CenterViewController: UIViewController, UIWebViewDelegate {
     }
     
     func webViewDidFinishLoad(webView: UIWebView) {
-        //var data = receiveData();
-        //println("data: \(data)")
+        //get the data in there somehow
+        println(webView.request!.URL!.lastPathComponent!)
         
         // should tell it which webView I am with some property
-        
-        // SCRIPTS
+        switch webView.request!.URL!.lastPathComponent!{
+        case visualizationNames[0]+".html":
+            transformDataForTreemapping(webView)
+        case visualizationNames[1]+".html":
+            transformDataForCirclepacking(webView)
+        case visualizationNames[2]+".html":
+            transformDataForWorddistance(webView)
+        default:
+            break
+        }
+    }
+    
+    func transformDataForTreemapping(webView: UIWebView){
         var treeScript = "var data7 = '{\"name\": \"all\",\"children\": [{\"name\": \"accountant\",\"children\": [{\"name\": \"accountant\", \"size\": 3938}]},{\"name\": \"cop\",\"children\": [{\"name\": \"cop\", \"size\": 743}]}]}'; renderChart(data7);"
-                
-        //var greenScript = ""
-        //var blueScript = ""
-        //var scripts = [treeScript, greenScript, blueScript]
-        //var numberOfViews = 3
-
-        webView.stringByEvaluatingJavaScriptFromString(treeScript)
         
-        //println("webViewDidFinishLoad")
+        webView.stringByEvaluatingJavaScriptFromString(treeScript)
+    }
+    
+    func transformDataForCirclepacking(webView: UIWebView){
+        var treeScript = "var data7 = '{\"name\": \"all\",\"children\": [{\"name\": \"accountant\",\"children\": [{\"name\": \"accountant\", \"size\": 3938}]},{\"name\": \"cop\",\"children\": [{\"name\": \"cop\", \"size\": 743}]}]}'; renderChart(data7);"
+        
+        webView.stringByEvaluatingJavaScriptFromString(treeScript)
+    }
+    
+    func transformDataForWorddistance(webView: UIWebView){
+        var wordScript = "var myData = '{\"name\": \"cat\",\"children\": [{\"name\": \"feline\", \"distance\": 0.6, \"size\": 44},{\"name\": \"dog\", \"distance\": 0.4, \"size\": 22},{\"name\": \"bunny\", \"distance\": 0.0, \"size\": 10},{\"name\": \"gif\", \"distance\": 1.0, \"size\": 55},{\"name\": \"tail\", \"distance\": 0.2, \"size\": 88},{\"name\": \"fur\", \"distance\": 0.7, \"size\": 50}]}'; renderChart(myData);"
+
+        webView.stringByEvaluatingJavaScriptFromString(wordScript)
     }
     
     @IBAction func searchClicked(sender: UIButton) {
