76dd19b7fe61d0335439f69a90184fce907e4fb0
Rosstin <asterazul@gmail.com>
2015-05-29 11:09:16 -0700

    Added Basic Chart Stuff for CenterViewController.swift

--- Spark Insights/CenterViewController.swift
+++ Spark Insights/CenterViewController.swift
@@ -14,7 +14,7 @@ protocol CenterViewControllerDelegate {
     optional func collapseSidePanels()
 }
 
-class CenterViewController: UIViewController {
+class CenterViewController: UIViewController, UIWebViewDelegate {
 
     var delegate: CenterViewControllerDelegate?
     
@@ -36,20 +36,56 @@ class CenterViewController: UIViewController {
 
     func setupScrollView() {
         var colors = [UIColor.blueColor(), UIColor.darkGrayColor(), UIColor.grayColor()]
+        
+        
+        // REQUESTS
+        let treeFilePath = NSBundle.mainBundle().URLForResource("Visualizations/treemap", withExtension: "html")
+        let treeRequest = NSURLRequest(URL: treeFilePath!)
+        
+        let greenFilePath = NSBundle.mainBundle().URLForResource("Visualizations/circlepacking", withExtension: "html")
+        let greenRequest = NSURLRequest(URL: greenFilePath!)
+        
+        let blueFilePath = NSBundle.mainBundle().URLForResource("Visualizations/blue", withExtension: "html")
+        let blueRequest = NSURLRequest(URL: blueFilePath!)
+        
+        var requests = [treeRequest, greenRequest, blueRequest]
+        
         var numberOfViews = 3
         
         for i in 0..<numberOfViews {
             var myOrigin = CGFloat(i) * self.dummyView.frame.size.width
             
-            var myView = UIView(frame: CGRectMake(myOrigin, 0, self.dummyView.frame.size.width, self.dummyView.frame.size.height))
-            myView.backgroundColor = colors[i % numberOfViews]
+            var myWebView = UIWebView(frame: CGRectMake(myOrigin, 0, self.dummyView.frame.size.width, self.dummyView.frame.size.height))
+            myWebView.backgroundColor = colors[i % numberOfViews]
             
-            self.scrollView.addSubview(myView)
+            myWebView.loadRequest(requests[i])
+            
+            myWebView.delegate = self
+            
+            self.scrollView.addSubview(myWebView)
         }
         
         self.scrollView.contentSize = CGSizeMake(self.dummyView.frame.size.width * CGFloat(numberOfViews), self.dummyView.frame.size.height)
     }
     
+    func webViewDidFinishLoad(webView: UIWebView) {
+        //var data = receiveData();
+        //println("data: \(data)")
+        
+        // should tell it which webView I am with some property
+        
+        // SCRIPTS
+        var treeScript = "var data7 = '{\"name\": \"all\",\"children\": [{\"name\": \"accountant\",\"children\": [{\"name\": \"accountant\", \"size\": 3938}]},{\"name\": \"cop\",\"children\": [{\"name\": \"cop\", \"size\": 743}]}]}'; renderChart(data7);"
+                
+        //var greenScript = ""
+        //var blueScript = ""
+        //var scripts = [treeScript, greenScript, blueScript]
+        //var numberOfViews = 3
+
+        webView.stringByEvaluatingJavaScriptFromString(treeScript)
+        
+        //println("webViewDidFinishLoad")
+    }
     
     @IBAction func searchClicked(sender: UIButton) {
         delegate?.toggleRightPanel?()
