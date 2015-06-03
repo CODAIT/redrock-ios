264079c9c6bbb7507048ce0643b0cb3b80689803
Barbara Gomes <barbaragomes@barbaras-mbp.usca.ibm.com>
2015-06-03 11:54:08 -0700

    Change refresh time

--- Spark Insights/CenterViewController.swift
+++ Spark Insights/CenterViewController.swift
@@ -144,7 +144,7 @@ class CenterViewController: UIViewController, UIWebViewDelegate, UIScrollViewDel
     func waitToUpdateSearch()
     {
         // 5min until new update be available
-        let delay = 5.0 * Double(NSEC_PER_SEC)
+        let delay = 300.0 * Double(NSEC_PER_SEC)
         let time = dispatch_time(DISPATCH_TIME_NOW, Int64(delay))
         dispatch_after(time, dispatch_get_main_queue()) {
             self.canUpdateSearch = true
