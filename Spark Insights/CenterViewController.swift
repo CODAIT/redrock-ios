83543cac7bc4c7db1a50428d901ea85e52ead6b8
Rosstin <asterazul@gmail.com>
2015-06-12 11:45:01 -0700

    timemap stops animating when you leave the page

--- Spark Insights/CenterViewController.swift
+++ Spark Insights/CenterViewController.swift
@@ -31,7 +31,7 @@ class CenterViewController: UIViewController, UIWebViewDelegate, UIScrollViewDel
     var visualizationHandler: VisualizationHandler = VisualizationHandler()
     
     // last visited page
-    var previousPage = 0
+    var previousPage : Int = 0
    
     //Can update search
     var canUpdateSearch = false
@@ -313,7 +313,18 @@ class CenterViewController: UIViewController, UIWebViewDelegate, UIScrollViewDel
         if(page >= Config.getNumberOfVisualizations()){
             page = Config.getNumberOfVisualizations()-1
         }
-        if(previousPage != page){
+        if(previousPage != page){ //page was changed
+            Log("page was changed from \(previousPage) to \(page)")
+            if previousPage == Config.visualizationsIndex.timemap.rawValue{
+                Log("left timemap so stop animation")
+                visualizationHandler.stopTimemap()
+            }
+            if page == Config.visualizationsIndex.timemap.rawValue{
+                Log("entered timemap so start animation")
+                visualizationHandler.startTimemap()
+            }
+            
+            
             previousPage = page
             //visualizationHandler.reloadAppropriateView(page)
             pageControlView.selectedIndex = page
