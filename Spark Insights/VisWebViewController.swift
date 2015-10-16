e71a06b1d6980dcb9b5e5c06378baf16114e8d39
Rosstin <asterazul@gmail.com>
2015-10-16 12:29:52 -0700

    force node graph closer to redlines

--- Spark Insights/VisWebViewController.swift
+++ Spark Insights/VisWebViewController.swift
@@ -385,6 +385,8 @@ class VisWebViewController: VisMasterViewController, VisLifeCycleProtocol, WKNav
                 //TODO: should have searchterm should be from actual searchterm
                 dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), {
                     
+                    
+                    
                     var script9 = "var myData = '{\"nodes\": [ {\"name\":\"\(self.searchText)\",\"value\":\(self.chartData[0][2]),\"group\":1}, " //the search text just arbitrarily takes the value of the first data point as its value
                     for r in 0..<self.self.chartData.count{
                         script9+="{\"name\": \""
